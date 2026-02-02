7be011263add6cdef45d8b01d3a566ef43454330
T0wti <diego.borto31@gmail.com>
2026-02-02 20:51:12 +0100

    chore : edit the Program.cs file to test the new interface

--- src/EasySave.Infrastructure/Program.cs
+++ src/EasySave.Infrastructure/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+// Code to dev the console (exclude only available here, don't push it)
+using EasySave.Console.Resources;
+using EasySave.Console.ConsoleUI;
+
+namespace EasySave.Infrastructure;
+
+class Program
+{
+    static void Main()
+    {
+        ITextProvider texts = new EnglishTextProvider();
+        var menu = new BaseMenu(texts);
+
+        menu.Display();
+    }
+}
