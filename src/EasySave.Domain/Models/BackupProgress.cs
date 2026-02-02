e9ad2d013bccc36b12a2335d85c75db6c741a429
Zargan <zargan.dc@gmail.com>
2026-02-02 12:01:35 +0100

    feat: add backup models, enums, and interfaces

---
+++ src/EasySave.Domain/Models/BackupProgress.cs
@@ -0,0 +1,34 @@
+﻿using EasySave.Domain.Enums;
+
+namespace EasySave.Domain.Models
+{
+    public class BackupProgress
+    {
+        public string BackupName { get; set; }
+        public BackupJobState State { get; set; }
+        public DateTime LastUpdate { get; internal set; }
+
+
+        public int TotalFiles { get; internal set; }
+        public long TotalSize { get; internal set; }
+
+        public int RemainingFiles { get; internal set; }
+        public long RemainingSize { get; internal set; }
+
+        public string? CurrentSourceFile { get; internal set; }
+        public string? CurrentTargetFile { get; internal set; }
+
+        private BackupProgress() { }
+
+        public static BackupProgress From(BackupJob job)
+        {
+            return new BackupProgress
+            {
+                BackupName = job.Name,
+                State = BackupJobState.Active,
+                LastUpdate = DateTime.Now
+            };
+        }
+    }
+}
+
