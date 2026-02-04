323202063e4d7f779fd49585aacaae43651a8a71
gaelsweden <gael.garridomoran@viacesi.fr>
2026-02-04 14:27:47 +0100

    Refactor: simplify code using inheritance

--- src/EasySave.Infrastructure/Logging/LogService.cs
+++ src/EasySave.Infrastructure/Logging/LogService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EasySave.Infrastructure
+namespace EasySave.Infrastructure.Logging
 {
     internal class LogService
     {
--- src/EasySave.Infrastructure/Services/BackupService.cs
+++ src/EasySave.Infrastructure/Services/BackupService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EasySave.Infrastructure
+namespace EasySave.Infrastructure.Services
 {
     internal class BackupService
     {
