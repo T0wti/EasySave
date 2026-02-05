ef08b7fdba03ed5d80819913baeaf4f18860d412
Zargan <zargan.dc@gmail.com>
2026-02-05 13:50:55 +0100

    feature: add easylog logic : service, writers and interfaces

---
+++ src/EasySave.EasyLog/EasyLogService.cs
@@ -0,0 +1,20 @@
+﻿using EasySave.EasyLog.Interfaces;
+using EasySave.EasyLog.Writers;
+
+namespace EasySave.EasyLog
+{
+    public class EasyLogService : ILogService
+    {
+        private readonly JsonLogWriter _writer;
+
+        public EasyLogService(string logDirectoryPath)
+        {
+            _writer = new JsonLogWriter(logDirectoryPath);
+        }
+
+        public void WriteJson(object entry)
+        {
+            _writer.WriteJson(entry);
+        }
+    }
+}
---
+++ src/EasySave.EasyLog/Interfaces/ILogService.cs
@@ -0,0 +1,8 @@
+﻿namespace EasySave.EasyLog.Interfaces
+{
+    public interface ILogService
+    {
+        void WriteJson(object entry);
+    }
+
+}
