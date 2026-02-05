4e48c055188dd91698989b3117054bd7327b2d1b
Zargan <zargan.dc@gmail.com>
2026-02-05 21:15:45 +0100

    refactor: move state attribute handling from BackupService to StateService

--- src/EasySave.Domain/Interfaces/IStateService.cs
+++ src/EasySave.Domain/Interfaces/IStateService.cs
@@ -4,8 +4,8 @@ namespace EasySave.Domain.Interfaces
 {
     public interface IStateService
     {
-        void Initialize(BackupProgress progress);
-        void Update(BackupProgress progress);
+        void Initialize(BackupProgress progress, List<FileDescriptor> files);
+        void Update(BackupProgress progress, FileDescriptor file, string targetPath);
         void Complete(int backupJobId);
         void Fail(int backupJobId);
     }
