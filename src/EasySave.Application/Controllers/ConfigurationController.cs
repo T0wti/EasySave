a15433a08c02ecd794d0e177ab7cdbd25c8ae45f
Zargan <zargan.dc@gmail.com>
2026-02-07 14:31:46 +0100

    feature:add the controllers in the application folder

---
+++ src/EasySave.Application/Controllers/ConfigurationController.cs
@@ -0,0 +1,28 @@
+﻿using EasySave.Domain.Interfaces;
+using EasySave.Domain.Models;
+using EasySave.Domain.Enums;
+
+namespace EasySave.Application.Controllers
+{
+    public class ConfigurationController
+    {
+        private readonly IConfigurationService _configService;
+
+        public ConfigurationController(IConfigurationService configService)
+        {
+            _configService = configService;
+        }
+
+        public ApplicationSettings Load()
+        {
+            return _configService.LoadSettings();
+        }
+
+        public void ChangeLanguage(Language lang)
+        {
+            var settings = _configService.LoadSettings();
+            settings.Language = lang;
+            _configService.SaveSettings(settings);
+        }
+    }
+}
---
+++ src/EasySave.Application/Controllers/ControllerFactory.cs
@@ -0,0 +1,41 @@
+﻿using EasySave.Domain.Interfaces;
+using EasySave.Domain.Services;
+using EasySave.EasyLog;
+
+namespace EasySave.Application.Controllers
+{
+    public static class ControllerFactory
+    {
+        public static BackupController CreateBackupController()
+        {
+            // 1. Charger la configuration (qui va créer le fichier par défaut si besoin)
+            IConfigurationService configService = new ConfigurationService();
+            var settings = configService.LoadSettings();
+
+            // 2. INITIALISATION DES SINGLETONS
+            // On passe les chemins récupérés depuis les settings
+            FileStateService.Instance.Initialize(settings.StateFileDirectoryPath);
+            EasyLogService.Instance.Initialize(settings.LogDirectoryPath); // <-- C'est l'étape manquante !
+
+            // 3. Instanciation des services classiques
+            IFileService fileService = new FileService();
+            IBackupStrategy fullStrategy = new FullBackupStrategy(fileService);
+            IBackupStrategy diffStrategy = new DifferentialBackupStrategy(fileService);
+            IFileBackupService fileBackupService = new FileBackupService();
+
+            IBackupManagerService manager = new BackupManagerService(fileBackupService);
+
+            // 4. Injection dans le BackupService
+            // Assure-toi que BackupService utilise bien les instances initialisées
+            IBackupService executor = new BackupService(fileService, fullStrategy, diffStrategy, fileBackupService);
+
+            return new BackupController(manager, executor);
+        }
+
+        public static ConfigurationController CreateConfigurationController()
+        {
+            IConfigurationService configService = new ConfigurationService();
+            return new ConfigurationController(configService);
+        }
+    }
+}
