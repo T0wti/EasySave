69a6fb15193dc4ab69879ea5cef6474e255946e4
Zargan <zargan.dc@gmail.com>
2026-02-08 16:45:18 +0100

    refactor: add progress in the state model and file

--- src/EasySave.Domain/Models/BackupProgress.cs
+++ src/EasySave.Domain/Models/BackupProgress.cs
@@ -1,4 +1,5 @@
 ﻿using EasySave.Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace EasySave.Domain.Models
 {
@@ -10,13 +11,25 @@ namespace EasySave.Domain.Models
         public DateTime LastUpdate { get; internal set; }
 
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public int TotalFiles { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public long TotalSize { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public int RemainingFiles { get; internal set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public long RemainingSize { get; internal set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public double Progression { get; internal set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? CurrentSourceFile { get; internal set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? CurrentTargetFile { get; internal set; }
 
         public BackupProgress() { }
@@ -28,7 +41,8 @@ namespace EasySave.Domain.Models
                 BackupJobId = job.Id,
                 BackupName = job.Name,
                 State = BackupJobState.Active,
-                LastUpdate = DateTime.Now
+                LastUpdate = DateTime.Now,
+                Progression = 0
             };
         }
     }
