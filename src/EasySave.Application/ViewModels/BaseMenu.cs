30af91989e670b5c53740c0fd8e29da641b25861
T0wti <diego.borto31@gmail.com>
2026-02-12 19:17:39 +0100

    feat : add base menu in GUI, need to do the front

---
+++ src/EasySave.Application/ViewModels/BaseMenu.cs
@@ -0,0 +1,26 @@
+﻿namespace EasySave.Application.ViewModels;
+
+public partial class BaseMenu : ViewModelBase
+{
+    public string Title { get; }
+    public string CreateBackup { get; }
+    public string DeleteBackup { get; }
+    public string EditBackup { get; }
+    public string ListBackup { get; }
+    public string ExeBackup { get; }
+    public string LogFormat { get; }
+    public string LanguageOption { get; }
+    
+    public BaseMenu()
+    {
+        Title = Texts.MainMenuTitle;
+        CreateBackup = Texts.CreateBackup;
+        DeleteBackup = Texts.DeleteBackup;
+        EditBackup = Texts.EditBackup;
+        ListBackup = Texts.ListBackup;
+        ExeBackup = Texts.ExeBackup;
+        LogFormat = Texts.LogFormat;
+        LanguageOption = Texts.LanguageOption;
+    }
+
+}
--- src/EasySave.Application/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EasySave.Application.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
