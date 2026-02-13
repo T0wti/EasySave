1c49a07414eef1845074a8f345e42451c1ef32ed
gaelsweden <gael.garridomoran@viacesi.fr>
2026-02-13 22:23:11 +0100

    feat: add highlighted choice in menu

--- src/EasySave.GUI/ViewModels/MainWindowViewModel.cs
+++ src/EasySave.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,9 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace EasySave.GUI.ViewModels;
 
-public class MainWindowViewModel : ObservableObject
+public partial class MainWindowViewModel : ObservableObject
 {
     private ViewModelBase _currentView;
 
@@ -11,10 +12,70 @@ public class MainWindowViewModel : ObservableObject
         get => _currentView;
         set => SetProperty(ref _currentView, value);
     }
+    //ObservableProperty automatically creates a public version of each private property below
+    [ObservableProperty] private bool _isBaseActive;
+    [ObservableProperty] private bool _isCreateActive;
+    [ObservableProperty] private bool _isEditActive;
+    [ObservableProperty] private bool _isDeleteActive;
+    [ObservableProperty] private bool _isListActive;
+    [ObservableProperty] private bool _isExecuteActive;
+    [ObservableProperty] private bool _isSettingsActive;
 
     public MainWindowViewModel()
     {
-        CurrentView = new BaseMenuViewModel(this);
+        //CurrentView = new BaseMenuViewModel(this);
+        CurrentView = new CreateBackupMenuViewModel(this);
     }
 
+    [RelayCommand]
+    public void NavigateToBaseMenu()
+    {
+        CurrentView = new BaseMenuViewModel(this);
+        ResetActiveStates();
+        IsBaseActive = true;
+    }
+
+    [RelayCommand]
+    public void NavigateToCreateBackup()
+    {
+        CurrentView = new CreateBackupMenuViewModel(this);
+        ResetActiveStates();
+        IsCreateActive = true;
+    }
+
+    //[RelayCommand]
+    //public void NavigateToDeleteBackup() => CurrentView = new DeleteBackupViewMenuModel(this);
+
+    //[RelayCommand]
+    //public void NavigateToEditBackup() => CurrentView = new EditBackupMenuViewModel(this);
+
+    [RelayCommand]
+    public void NavigateToListBackup()
+    {
+        CurrentView = new ListBackupMenuViewModel(this);
+        ResetActiveStates();
+        IsListActive = true;
+    }
+
+    //[RelayCommand]
+    //public void NavigateToExecuteBackup() => CurrentView = new ExecuteBackupMenuViewModel(this);
+
+    [RelayCommand]
+    public void NavigateToSettings()
+    {
+        CurrentView = new SettingsMenuViewModel(this);
+        ResetActiveStates();
+        IsSettingsActive = true;
+    }
+
+    private void ResetActiveStates()
+    {
+        IsBaseActive = false;
+        IsCreateActive = false;
+        IsEditActive = false;
+        IsDeleteActive = false;
+        IsListActive = false;
+        IsExecuteActive = false;
+        IsSettingsActive = false;
+    }
 }
