0dd03c64e45bfcf28cb2509d166a0b280b7102e2
T0wti <diego.borto31@gmail.com>
2026-02-16 17:31:31 +0100

    fix: remove using of easysave.easylog in SettingsMenuViewModel.cs and correct by using the right int

--- src/EasySave.GUI/ViewModels/SettingsMenuViewModel.cs
+++ src/EasySave.GUI/ViewModels/SettingsMenuViewModel.cs
@@ -1,7 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using EasySave.Application.Resources;
-using EasySave.EasyLog.Enums;
 using System.Windows.Input;
 using Tmds.DBus.Protocol;
 
@@ -51,7 +50,7 @@ public partial class SettingsMenuViewModel : ViewModelBase
         else IsLanguage2Selected = true;
 
         // Check which log format is selected
-        if (currentFormat == LogFormat.Json) IsLogFormat1Selected = true;
+        if (currentFormat == 0) IsLogFormat1Selected = true;
         else IsLogFormat2Selected = true;
 
         // Handle the language change
