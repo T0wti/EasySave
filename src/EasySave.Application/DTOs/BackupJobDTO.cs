465511d9be3fa1504c77d341a6f7ad4120e339d2
Zargan <zargan.dc@gmail.com>
2026-02-17 18:34:01 +0100

    refactor: add correct namespace in application

--- src/EasySave.Application/DTOs/BackupJobDTO.cs
+++ src/EasySave.Application/DTOs/BackupJobDTO.cs
@@ -1,10 +1,12 @@
-﻿namespace EasySave.Application.DTOs;
-
-public class BackupJobDTO
+﻿namespace EasySave.Application.DTOs
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string SourcePath { get; set; }
-    public string TargetPath { get; set; }
-    public string Type { get; set; }
+
+    public class BackupJobDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+        public string Type { get; set; }
+    }
 }
--- src/EasySave.Application/Resources/ITextProvider.cs
+++ src/EasySave.Application/Resources/ITextProvider.cs
@@ -1,100 +1,98 @@
-namespace EasySave.Application.Resources;
-
-public interface ITextProvider
+namespace EasySave.Application.Resources
 {
-    // General content
-    string Header { get; }
-    string Footer { get; }
-    string MainMenuTitle { get; }
-    string AskEntryFromUser { get; }
-    string ExitOption { get; }
-    string WrongInput { get; }
-    string Exit { get; }
-    string Confirm { get; }
-    string Home { get; }
-    string Save { get; }
-    
-    // Home Page
-    string HomeWelcome { get; }
-    string HomeConfiguredBackup { get; }
-    
-    // Options in the base menu | Argan
-    string DeleteBackup { get; }
-    string CreateBackup { get; }
-    string EditBackup { get; }
-    string LanguageOption { get; }
-    string ListBackup { get; }
-    string ExeBackup { get; }
-    string LogFormat { get; }
-    string SettingsMenu { get; }
-    
-    // Options for the creation 
-    string CreateBackupMenuTitle { get; }
-    string EnterBackupName { get; }
-    string EnterSourcePath { get; }
-    string EnterTargetPath { get; }
-    string EnterBackupType { get; }
-    string WaterMarkBackupName { get; }
-    string WaterMarkBackupSourcePath { get; }
-    string WaterMarkBackupTargetPath { get; }
-    string Full { get; }
-    string Differential { get; }
-    string ExtensionToEncryptTitle { get; }
-    string Encrypt { get; }
 
-    //
+    public interface ITextProvider
+    {
+        // General content
+        string Header { get; }
+        string Footer { get; }
+        string MainMenuTitle { get; }
+        string AskEntryFromUser { get; }
+        string ExitOption { get; }
+        string WrongInput { get; }
+        string Exit { get; }
+        string Confirm { get; }
+        string Home { get; }
+        string Save { get; }
 
-    string BackupCreated { get; }
-    string BackupEdited { get; }
-    string EnterBackupToDelete { get; }
-    string BackupDeleted { get; }
-    string AskIdToEdit { get; }
-    string BrowseFile { get; }
+        // Home Page
+        string HomeWelcome { get; }
+        string HomeConfiguredBackup { get; }
 
-    // Options in the change language menu | Le
-    string LanguageMenuTitle { get; }
-    string Language1  { get; }
-    string Language2 { get; }
-    
-    // Options in the first start menu | Loup
-    string ChooseFirstLanguageMenuTitle { get; }
-    string ChooseFirstLanguage { get; }
-    
-    // List Backup Menu
-    string ListBackupMenuTitle { get; }
-    
-    // Backup Detail Menu
-    string BackupNameMenuTitle { get; }
-    string BackupName { get; }
-    string BackupSourcePath { get; }
-    string BackupTargetPath { get; }
-    string BackupType { get; }
-    
-    // Execute Backup Menu
-    string ExeBackupMenuTitle { get; }
-    string BackupNames { get; }
-    string ExeBackupInstruction { get; }
-    
-    // Execute Backup Menu Details
-    string ExeBackupMenuDetailTitle { get; }
-    string ExeBackupInProgress { get; }
-    string ExeBackupCompleted { get; }
+        // Options in the base menu | Argan
+        string DeleteBackup { get; }
+        string CreateBackup { get; }
+        string EditBackup { get; }
+        string LanguageOption { get; }
+        string ListBackup { get; }
+        string ExeBackup { get; }
+        string LogFormat { get; }
+        string SettingsMenu { get; }
 
-    // Change Log Format Menu
-    string LogFormatMenuTitle { get; }
-    string LogFormat1 { get; }
-    string LogFormat2 { get; }
-    string LogFormatChanged { get; }
-    string CurrentLogFormat { get; }
-    
-    // Settings menu
-    string SettingsMenuTitle { get; }
-    string SettingsMenuBusiness { get; }
-    string SettingsMenuLanguage { get; }
-    string SettingsMenuLogFormat { get; }
-    string SettingsMenuExemple { get; }
+        // Options for the creation 
+        string CreateBackupMenuTitle { get; }
+        string EnterBackupName { get; }
+        string EnterSourcePath { get; }
+        string EnterTargetPath { get; }
+        string EnterBackupType { get; }
+        string WaterMarkBackupName { get; }
+        string WaterMarkBackupSourcePath { get; }
+        string WaterMarkBackupTargetPath { get; }
+        string Full { get; }
+        string Differential { get; }
+        string ExtensionToEncryptTitle { get; }
+        string Encrypt { get; }
+
+        //
+
+        string BackupCreated { get; }
+        string BackupEdited { get; }
+        string EnterBackupToDelete { get; }
+        string BackupDeleted { get; }
+        string AskIdToEdit { get; }
+        string BrowseFile { get; }
+
+        // Options in the change language menu | Le
+        string LanguageMenuTitle { get; }
+        string Language1 { get; }
+        string Language2 { get; }
+
+        // Options in the first start menu | Loup
+        string ChooseFirstLanguageMenuTitle { get; }
+        string ChooseFirstLanguage { get; }
+
+        // List Backup Menu
+        string ListBackupMenuTitle { get; }
+
+        // Backup Detail Menu
+        string BackupNameMenuTitle { get; }
+        string BackupName { get; }
+        string BackupSourcePath { get; }
+        string BackupTargetPath { get; }
+        string BackupType { get; }
+
+        // Execute Backup Menu
+        string ExeBackupMenuTitle { get; }
+        string BackupNames { get; }
+        string ExeBackupInstruction { get; }
+
+        // Execute Backup Menu Details
+        string ExeBackupMenuDetailTitle { get; }
+        string ExeBackupInProgress { get; }
+        string ExeBackupCompleted { get; }
+
+        // Change Log Format Menu
+        string LogFormatMenuTitle { get; }
+        string LogFormat1 { get; }
+        string LogFormat2 { get; }
+        string LogFormatChanged { get; }
+        string CurrentLogFormat { get; }
+
+        // Settings menu
+        string SettingsMenuTitle { get; }
+        string SettingsMenuBusiness { get; }
+        string SettingsMenuLanguage { get; }
+        string SettingsMenuLogFormat { get; }
+        string SettingsMenuExemple { get; }
+    }
 }
-
-
-
-
