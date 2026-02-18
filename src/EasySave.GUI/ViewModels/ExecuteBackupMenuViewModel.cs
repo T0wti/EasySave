536ddcb018a6f21b9b00d2dd23059c3609b9015e
T0wti <diego.borto31@gmail.com>
2026-02-18 01:40:03 +0100

    fix: remove useless coms

--- src/EasySave.GUI/ViewModels/ExecuteBackupMenuViewModel.cs
+++ src/EasySave.GUI/ViewModels/ExecuteBackupMenuViewModel.cs
@@ -1,5 +1,5 @@
 using System.Collections.ObjectModel;
-using System.Linq; // Nécessaire pour le .Select et .Where
+using System.Linq;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 using EasySave.Application.DTOs;
@@ -23,26 +23,22 @@ namespace EasySave.GUI.ViewModels
         {
             Title = Texts.ExeBackupMenuTitle;
             Exit = Texts.Exit;
-
-            // Transformation des DTOs en Wrappers lors du chargement
+            
             var jobs = BackupAppService.GetAll()
                 .Select(dto => new BackupJobSelectionViewModel(dto));
             
             BackupJobs = new ObservableCollection<BackupJobSelectionViewModel>(jobs);
 
-            // Initialisation des commandes
             ExitCommand = new RelayCommand(NavigateToBase);
             ExecuteSelectedCommand = new RelayCommand(ExecuteSelectedJobs);
         }
 
         private void ExecuteSelectedJobs()
         {
-            // On filtre pour ne garder que ceux dont IsSelected est true
             var jobsToExecute = BackupJobs.Where(x => x.IsSelected).ToList();
 
             foreach (var selection in jobsToExecute)
             {
-                // On accède au DTO via la propriété .Job du wrapper
                 BackupAppService.ExecuteBackup(selection.Job.Id);
             }
         }
