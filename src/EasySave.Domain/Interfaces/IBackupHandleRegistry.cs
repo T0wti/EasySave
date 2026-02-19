0e925bbc3254675320e49a141a5588bcd049bba6
Zargan <zargan.dc@gmail.com>
2026-02-19 22:19:00 +0100

    feature : add registry handle to stock the the handle and the state of the thread

---
+++ src/EasySave.Domain/Interfaces/IBackupHandleRegistry.cs
@@ -0,0 +1,13 @@
+﻿namespace EasySave.Domain.Interfaces
+{
+    // Thread-safe registry of active backup job handles.
+    public interface IBackupHandleRegistry
+    {
+        void Register(int jobId, IBackupJobHandle handle);
+        void Unregister(int jobId);
+
+        IBackupJobHandle? Get(int jobId);
+
+        IEnumerable<(int JobId, IBackupJobHandle Handle)> GetAll();
+    }
+}
